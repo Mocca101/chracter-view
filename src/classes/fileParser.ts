1e494d78cad433f8e187dc66c52fdcb326c2f71e
Mocca101 <49754596+Mocca101@users.noreply.github.com>
2024-01-09 21:22:57 +0100

    Wip better parsed headings

--- src/classes/fileParser.ts
+++ src/classes/fileParser.ts
@@ -1,7 +1,3 @@
-// Output Object:
-// - statblock 
-// - description
-
 import { parseYaml } from "obsidian";
 import { zStatblock, type Statblock } from "../types/zod/zodSchemas";
 
@@ -21,66 +17,115 @@ type ParsedFile = {
 }
 
 export function parseFile(fileString: string) : ParsedFile {
+    console.log('start parsing in fileParser.ts');
+    
     const sections = parseSections(fileString);
-    console.log(sections);
+    console.log('sections', sections);
     const statblock = parseStatblock(sections);
-    console.log(statblock);
     const description = parseDescription(sections);
-    console.log(description);
     return { statblock, description };
 }
 
-type Section = {
-    type: 'yaml' | 'code' | 'heading';
+type BaseSection = {
     lines: string[];
 }
 
+type CodeSection = BaseSection & {
+    type: 'code';
+    language?: string;
+}
+type YamlSection = BaseSection & {
+    type: 'yaml';
+}
+type HeadingSection = BaseSection & {
+    type: 'heading';
+    level: number;
+    subsections: Section[];
+    parentSection?: HeadingSection;
+}
+
+type ParagraphSection = BaseSection & { 
+    type: 'paragraph';
+}
+
+type Section = CodeSection | YamlSection | HeadingSection | ParagraphSection;
+
 function parseSections(fileString: string) : Section[] {
     const lines = fileString.split('\n');
-    const sections: Section[] = [];
-    let currentHeading: Section = null;
-    let currentBlock: Section = null;
-    let inBlock = false;
+    let currentHeading: HeadingSection = { type: 'heading', lines: [], level: 0, subsections: [] };
+    let currentBlock: Section = currentHeading;
+    const sections: Section[] = [currentBlock];
+
+    // TODO: refactor this to be more readable
+    // TODO: add blocks & headings to parent headings
+
+    console.log(currentBlock)
 
     for(let i = 0; i < lines.length; i++) {
         const line = lines[i];
         let sectionType = parseLineForSection(line);
 
-        if(sectionType === 'yaml' && i > 0 && currentBlock.type !== 'yaml') sectionType = null;
+
+        if(sectionType === 'yaml' && i > 0 && currentBlock?.type !== 'yaml') sectionType = null;
 
         if(!sectionType) {
             currentBlock?.lines.push(line);
-            currentHeading?.lines.push(line);
             continue;
         }
 
         if(sectionType === 'heading') {
-            currentHeading = { type: 'heading', lines: [line] };
-            sections.push(currentHeading);
+            const level = line.match(/^#+/)[0].length;
+
+            const newHeading: HeadingSection = { type: 'heading', lines: [line], level, subsections: [] };
+
+            if(level > currentHeading.level) {
+                currentHeading.subsections.push(newHeading);
+                newHeading.parentSection = currentHeading;
+                currentHeading = newHeading;
+
+                console.log('level < currentHeading.level', currentHeading);
+            } 
+            // else if(level === currentHeading.level) {
+            //     currentHeading.parentSection.subsections.push(newHeading);
+            //     newHeading.parentSection = currentHeading.parentSection;
+            //     currentHeading = newHeading;            
+            // } 
+            else {
+                while(level < currentHeading.level) {
+                    currentHeading = currentHeading.parentSection;
+                    console.log('level < currentHeading.level', currentHeading);
+                }
+                currentHeading.subsections.push(newHeading);
+                newHeading.parentSection = currentHeading;
+                currentHeading = newHeading;
+            }            
+
+            currentBlock = currentHeading;
+            sections.push(currentBlock);
             continue;
         }
 
         if(sectionType === 'code') {
-            if(currentBlock) {
+            if(currentBlock.type === 'code') {
                 currentBlock.lines.push(line);
-                currentHeading?.lines.push(line);
-                currentBlock = null;
+                currentBlock = currentHeading;
                 continue;
             } 
             currentBlock = { type: 'code', lines: [line] };
+            currentHeading.subsections.push(currentBlock);
             sections.push(currentBlock);
             continue;
         }
 
         if(sectionType === 'yaml') {
-            if(currentBlock) {
+            if(currentBlock.type === 'yaml') {
                 currentBlock.lines.push(line);
-                currentHeading?.lines.push(line);
-                currentBlock = null;
+                currentBlock = currentHeading;
                 continue;
             }
 
             currentBlock = { type: 'yaml', lines: [line] };
+            currentHeading.subsections.push(currentBlock);
             sections.push(currentBlock);
             continue;
         }
